dd33211016f46ca3e0adda14e5f39142b8f4cbf2
Thomas Davis <thomasalwyndavis@gmail.com>
2014-06-13 18:32:03 +1000

    w

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 resumes
 =======
 http://microformats.org/wiki/hresume
+
+
+## Getting started
+
+```
+node resume.js thomasdavis.json thomasdavis.html
+```
+
+Everyone just work on that resume and print out the html and shit and leave comments and get an idea of shit blah
--- resume.js
+++ resume.js
@@ -3,9 +3,12 @@ var Mustache = require('mustache');
 var _ = require('lodash');
 
 
+var resumeInput = process.argv[2]; //value will be "banana"
+var resumeOutput = process.argv[3];
+
 var resumeTemplate = fs.readFileSync('layout.template', 'utf8');
-var resumeData = JSON.parse(fs.readFileSync('thomasdavis.json', 'utf8'));
+var resumeData = JSON.parse(fs.readFileSync(resumeInput, 'utf8'));
 
 var resumeHTML = Mustache.render(resumeTemplate, resumeData);
 
-fs.writeFileSync('thomasdavis.html', resumeHTML, 'utf8');
+fs.writeFileSync(resumeOutput, resumeHTML, 'utf8');
