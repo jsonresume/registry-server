1ebcb9f8eef809bf50590d3079454f1689ad04bf
thomasdavis <thomasalwyndavis@gmail.com>
2014-06-15 17:54:37 +1000

    ASDg

--- server.js
+++ server.js
@@ -84,6 +84,7 @@ app.get('/resume/:uid.:format', function(req, res) {
 });
 app.post('/resume', function (req, res) {
 	var uid = guid();
+	console.log(req.body);
 	resumes[uid] = req.body && JSON.parse(req.body.resume) || {};
 	res.send({url:'http://resume-json.herokuapp.com/resume/'+uid});
 });
