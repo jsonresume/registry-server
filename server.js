d0392f523f7e9a31d3e18b9d63a23d944c4933f7
thomasdavis <thomasalwyndavis@gmail.com>
2014-06-15 17:52:41 +1000

    ASDg

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ var resumeToHTML = require('resume-to-html');
 
 var app = express();
 var fs = require('fs');
-
-app.get('/resume/:username.:format', function(req, res) {
+var guid = (function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  }
+  return function() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+           s4() + '-' + s4() + s4() + s4();
+  };
+})();
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.get('/resume/user/:username.:format', function(req, res) {
 	var resume = JSON.parse(fs.readFileSync(req.params.username + '.json', 'utf8'));
 	console.log(req.params.format);
   	var format = req.params.format;
@@ -32,11 +47,46 @@ app.get('/resume/:username.:format', function(req, res) {
   			resumeToHTML(resume, function (content){
   				res.send(content);
   			});
-  		
+
   	}
 
 });
+var resumes = {};
+app.get('/resume/:uid.:format', function(req, res) {
+	console.log(resumes);
+	var resume = resumes[req.params.uid];
+	console.log(req.params.format);
+  	var format = req.params.format;
 
+  	var content = '';
+  	switch(format) {
+  		case 'json':
+  			content =  JSON.stringify(resume, undefined, 4);
+  			res.send(content);
+  			break;
+  		case 'txt':
+  			content = resumeToText.resumeToText(resume, function (plainText){
+  				res.set({'Content-Type': 'text/plain',
+  					'Content-Length': plainText.length});
+
+  				res.set('Cba', 'text/plain');
+  				res.type('text/plain')
+				res.send(200,plainText);
+  			});
+  			break
+  		default:
+  			resumeToHTML(resume, function (content){
+  				res.send(content);
+  			});
+
+  	}
+
+});
+app.get('/resume', function (req, res) {
+	var uid = guid();
+	resumes[uid] = req.body && JSON.parse(req.body.resume) || {};
+	res.send({url:'http://resume-json.herokuapp.com/resume/'+uid});
+});
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
