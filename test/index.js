457d8d55e63c5a941b2aa7418a27934f9032da55
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-07-09 23:15:46 -0400

    testing that results are parseable json(ld)

--- test/index.js
+++ test/index.js
@@ -11,41 +11,57 @@ var user = {
 };
 
 test('JSON-LD context serving', function(t) {
-    t.plan(1);
+    t.plan(2);
     request
         .get('http://localhost:5000/')
         .set('Accept', 'application/ld+json')
+        .buffer(true)
         .end(function(err, res){
             t.notOk(err, 'no error');
+            t.doesNotThrow(function(){ JSON.parse(res.text) }, "JSON");
         });
 });
 
 
 test('new user creation', function(t) {
-    t.plan(1);
+    t.plan(2);
     request
         .post('http://localhost:5000/user')
         .send(user)
         .end(function(err, res){
             t.notOk(err, 'no error');
+            t.doesNotThrow(function(){ JSON.parse(res.text) }, "JSON");
         });
 });
 
 test('resume posting', function(t) {
-    t.plan(1);
+    t.plan(2);
     request
         .post('http://localhost:5000/resume')
         .send(_.extend({resume: schema.resumeJson}, user))
         .end(function(err, res){
             t.notOk(err, 'no error');
+            t.doesNotThrow(function(){ JSON.parse(res.text) }, "JSON");
         });
 });
 
 test('resume fetching', function(t) {
-    t.plan(1);
+    t.plan(2);
     request
-        .get('http://localhost:5000/richardhendricks')
+        .get('http://localhost:5000/richardhendricks.json')
         .end(function(err, res){
             t.notOk(err, 'no error');
+            t.doesNotThrow(function(){ JSON.parse(res.text); }, "JSON");
+        });
+});
+
+test('resume fetching (as json-ld)', function(t) {
+    t.plan(2);
+    request
+        .get('http://localhost:5000/richardhendricks.jsonld')
+        .buffer(true)
+        .end(function(err, res){
+            t.notOk(err, 'no error');
+            t.doesNotThrow(function(){ JSON.parse(res.text); }, "JSON");
         });
 });
