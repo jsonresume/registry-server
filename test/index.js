ac936838efab2b0f5f23e3e48ea9b5182ade0ced
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-07-09 21:49:22 -0400

    adding baseline tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,51 @@
 var test = require('tape');
+var request = require('superagent');
+var _ = require('lodash');
 
-test('Server blank test', function(t) {
+var schema = require('resume-schema');
+
+var user = {
+    username: 'richardhendricks',
+    email: 'richardhendricks@example.com',
+    password: 'password'
+};
+
+test('JSON-LD context serving', function(t) {
     t.plan(1);
-    t.ok(true, 'blank test');
+    request
+        .get('http://localhost:5000/')
+        .set('Accept', 'application/ld+json')
+        .end(function(err, res){
+            t.notOk(err, 'no error');
+        });
 });
 
-//
+
+test('new user creation', function(t) {
+    t.plan(1);
+    request
+        .post('http://localhost:5000/user')
+        .send(user)
+        .end(function(err, res){
+            t.notOk(err, 'no error');
+        });
+});
+
+test('resume posting', function(t) {
+    t.plan(1);
+    request
+        .post('http://localhost:5000/resume')
+        .send(_.extend({resume: schema.resumeJson}, user))
+        .end(function(err, res){
+            t.notOk(err, 'no error');
+        });
+});
+
+test('resume fetching', function(t) {
+    t.plan(1);
+    request
+        .get('http://localhost:5000/richardhendricks')
+        .end(function(err, res){
+            t.notOk(err, 'no error');
+        });
+});
