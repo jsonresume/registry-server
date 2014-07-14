60a894694edc96d675a88cee9eb2a33cdfab7da4
Roland Sharp <rolandnsharp@gmail.com>
2014-07-14 22:30:32 +1000

    session checking for theme change now working

--- server.js
+++ server.js
@@ -283,6 +283,8 @@ MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
     // update theme
     app.put('/resume', function(req, res) {
 
+        console.log(req.body);
+
         var password = req.body.password;
         var email = req.body.email;
         var theme = req.body.theme;
@@ -291,31 +293,61 @@ MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
         db.collection('users').findOne({
             'email': email
         }, function(err, user) {
-            if (user && bcrypt.compareSync(password, user.hash)) {
 
-                db.collection('resumes').update({
-                    //query
-                    'jsonresume.username': user.username
-                }, {
-                    // update set new theme
-                    $set: {
-                        'jsonresume.theme': theme
-                    }
-                }, {
-                    //options
-                    upsert: true,
-                    safe: true
-                }, function(err, resume) {
-                    res.send({
-                        url: 'http://registry.jsonresume.org/' + user.username
+            redisClient.get(req.body.session, function(err, valueExists) {
+                console.log(err, valueExists, 'theme redis');
+
+                if (!valueExists && user && bcrypt.compareSync(password, user.hash)) {
+
+                    db.collection('resumes').update({
+                        //query
+                        'jsonresume.username': user.username
+                    }, {
+                        // update set new theme
+                        $set: {
+                            'jsonresume.theme': theme
+                        }
+                    }, {
+                        //options
+                        upsert: true,
+                        safe: true
+                    }, function(err, resume) {
+                        res.send({
+                            url: 'http://registry.jsonresume.org/' + user.username
+                        });
                     });
-                });
-            } else {
-                console.log('deleted');
-                res.send({
-                    message: 'authentication error'
-                });
-            }
+                } else if (valueExists === null) {
+                    res.send({
+                        sessionError: 'invalid session'
+                    });
+                } else if (valueExists === 'true') {
+                    console.log('redis session success');
+                    db.collection('resumes').update({
+                        //query
+                        'jsonresume.username': user.username
+                    }, {
+                        // update set new theme
+                        $set: {
+                            'jsonresume.theme': theme
+                        }
+                    }, {
+                        //options
+                        upsert: true,
+                        safe: true
+                    }, function(err, resume) {
+                        res.send({
+                            url: 'http://registry.jsonresume.org/' + user.username
+                        });
+                    });
+
+
+                } else {
+                    console.log('deleted');
+                    res.send({
+                        message: 'authentication error'
+                    });
+                }
+            });
         });
 
     });
