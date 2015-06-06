08d743d44b0a2157209e24224244bb58fa281976
Roland Sharp <rolandnsharp@gmail.com>
2015-06-06 20:49:27 +1000

    Add some error handling to stats controller.

--- controller/stats.js
+++ controller/stats.js
@@ -1,12 +1,18 @@
-module.exports = function stats(req, res) {
+module.exports = function stats(req, res, next) {
 
     var db = req.db
     var redis = req.redis
 
 
     redis.get('views', function(err, views) {
-        db.collection('users').find().count(function(e, usercount) {
-            db.collection('resumes').find().count(function(e, resumecount) {
+        db.collection('users').find().count(function(err, usercount) {
+            if (err) {
+                return next(err);
+            }
+            db.collection('resumes').find().count(function(err, resumecount) {
+                if (err) {
+                    return next(err);
+                }
                 res.send({
                     userCount: usercount,
                     resumeCount: resumecount,
