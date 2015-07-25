7068a927fc71460c51f006aead773cc70fe5b5d5
Roland Sharp <rolandnsharp@gmail.com>
2015-07-25 17:48:34 +1000

    Set mongoose schema for user.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({}, { strict: false });
+var userSchema = new Schema({
+  username: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  hash: { // TODO make virtual
+    type: String
+  }
+});
+
 
 module.exports = mongoose.model('User', userSchema);
